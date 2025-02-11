66b5fa408296ca62b6b9418b9ae76428e01aa781
RyoTani0210 <ryo.taniguchi@gmail.com>
2025-02-11 20:33:55 +0900

    パラメタライズしてみた

--- src/hotelplainsphere/test.py
+++ src/hotelplainsphere/test.py
@@ -4,31 +4,25 @@ from playwright.sync_api import Page
 from toppage import HotelPlanispherePage  #`HotelPlanispherePage` クラスがあるファイルをimport
 
 
-def test_all_buttons_navigation(page: Page, test_data):
+@pytest.mark.parametrize("button_name, click_method, expected_path", [
+    ("home", "click_home", "index.html"),
+    ("reservation", "click_reservation", "plans.html"),
+    ("register", "click_register", "signup.html"),
+    ("login", "click_login", "login.html")
+])
+def test_all_buttons_navigation(page: Page, test_data, button_name, click_method, expected_path):
     """
-    すべてのボタンをクリックした後のURLが期待値と一致するか確認
+    すべてのボタンをクリックした後のURLが期待値と一致するか確認（パラメータ化）
     """
-
-    #準備
+    # 準備
     hotel_page = HotelPlanispherePage(page, test_data)
     hotel_page.open_page()
 
-    # ホーム
-    hotel_page.click_home()
-    assert page.url == test_data["url"] + "index.html"
+    # ボタンをクリック（`getattr` を使って動的にメソッドを呼び出す）
+    getattr(hotel_page, click_method)()
 
-    # 宿泊予約
-    hotel_page.click_reservation()
-    assert page.url == test_data["url"] + "plans.html"
-
-    # 会員登録
-    hotel_page.click_register()
-    assert page.url == test_data["url"] + "signup.html"
-
-    # ログイン
-    hotel_page.click_login()
-    assert page.url == test_data["url"] + "login.html"
-
-    
+    # 遷移後のURLを検証
+    expected_url = test_data["url"] + expected_path
+    assert page.url == expected_url
 
 
